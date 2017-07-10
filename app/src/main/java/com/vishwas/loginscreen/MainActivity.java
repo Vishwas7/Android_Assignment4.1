48949214adb67d3d4b00d2f3bd99364f301cb1b6
Vishwas7 <svishwas788@gmail.com>
2017-07-10 14:55:12 +0530

    LoginScreen Assignment

---
+++ app/src/main/java/com/vishwas/loginscreen/MainActivity.java
@@ -0,0 +1,24 @@
+package com.vishwas.loginscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+}
+
